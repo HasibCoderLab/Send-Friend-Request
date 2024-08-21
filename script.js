40664c84380343285555a8ef2281ad4e0e20f4bd
[mdhasibhasan <[hffhhasib@gmail.com]>
2024-08-21 20:45:43 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var istatus =   document.querySelector("h5")
+var btn = document.querySelector("#add")
+var cheke = 0
+
+
+btn.addEventListener("click",function(){
+    if(cheke == 0){
+        istatus.innerHTML =  "Friends"
+        istatus.style.color = "green";
+        btn.innerHTML = "Remove Friend " 
+
+     cheke = 1
+    }
+
+    else{
+        istatus.innerHTML =   " Add  Friend  "
+        istatus.style.color = "red"
+        btn.innerHTML = "Add Friend " 
+     cheke =  0
+    }
+}) 
+
