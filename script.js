3abdea000060f2cff38b7fd05ce1048754c0961f
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-01 23:03:00 +0600

    update repo

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-var istatus =   document.querySelector("h5")
+var istatus = document.querySelector("h5")
 var btn = document.querySelector("#add")
 var cheke = 0
 
 
-btn.addEventListener("click",function(){
-    if(cheke == 0){
-        istatus.innerHTML =  "Friends"
+btn.addEventListener("click", function () {
+    if (cheke == 0) {
+        istatus.innerHTML = "Friends"
         istatus.style.color = "green";
-        btn.innerHTML = "Remove Friend " 
+        btn.innerHTML = "Remove Friend "
 
-     cheke = 1
+        cheke = 1
     }
 
-    else{
-        istatus.innerHTML =   " Add  Friend  "
+    else {
+        istatus.innerHTML = " Add  Friend  "
         istatus.style.color = "red"
-        btn.innerHTML = "Add Friend " 
-     cheke =  0
+        btn.innerHTML = "Add Friend "
+        cheke = 0
     }
-}) 
+})
 
